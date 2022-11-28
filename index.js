d24db0b224507371f39bd589ff750a549d9222ea
Taffie Theuri <taffietheuri4@gmail.com>
2022-11-28 13:49:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// Code your solutions in this file
+
+function writeCards(name, event) {
+  let card = [];
+    for (let i = 0; i < name.length; i++) {
+        card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return card;
+}
+
+
+function countDown (i) {
+    while (i >=0) {
+        console.log(i--);
+    }
+}
